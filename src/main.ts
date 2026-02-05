abb3fbce853bc77d7943607a7d3810fda3e535f8
LenilsonLima <lenilson.pantoja@estudante.ifms.edu.br>
2026-02-05 17:50:10 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,11 @@ import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 👇 COLOCA AQUI
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://autoflex-front.onrender.com',
-    ],
+    origin: [/^http:\/\/localhost:\d+$/, /^https:\/\/autoflex-front\.onrender\.com$/],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false, // deixe true só se usar cookie/sessão
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
